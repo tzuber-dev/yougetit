47fccb1c9c3d7a70e401138379c321d0ae1b61c3
tzuber-dev <zt50481@mail.harpercollege.edu>
2026-04-05 22:43:31 -0500

    Update hello.hpp

--- src/hello.hpp
+++ src/hello.hpp
@@ -1,3 +1,38 @@
 #include <string>
 
-std::string hello();
+#include <vector>
+#include <iostream>
+using namespace std;
+
+
+struct Student {
+    string firstname;
+    string lastname;
+    string studentId;
+
+};
+
+struct Assignment {
+    string name;
+    int sumofpoints;
+
+};
+
+
+class Gradebook {
+private:
+
+    vector<Student> students;
+    vector<Assignment> assignments;
+    vector<vector<int>> grade;
+
+    int findstudentindex(const std::string& studentId) const;
+    int findassignmentindex(const std::string& nameofassignment) const;
+public:
+    bool addstudent(const string& firstname,const string& lastname, const string& studentId);
+    bool addassignment(const string& nameofassignment, int sumofpoints);
+    bool enteryourgrade(const string studentId, const string& nameofassignment, int grade);
+    string report() const;
+
+
+};
