9f8f1c7c3847d862ba9a576c70cd43611c3deb95
tzuber-dev <zt50481@mail.harpercollege.edu>
2026-04-05 22:49:49 -0500

    Update hello_test.cpp

--- tests/hello_test.cpp
+++ tests/hello_test.cpp
@@ -4,7 +4,30 @@
 #include <catch2/generators/catch_generators_range.hpp>
 
 #include "../src/hello.hpp"
+#include <string>
 
-TEST_CASE( "it returns Hello World" ) {
-    REQUIRE( hello() == "Hello World!" );
+
+TEST_CASE("Works successfully with three students in a class") {
+
+     Gradebook gradebook;
+     gradebook.addstudent("Bob"," ""Bobberson","ABC123");
+     gradebook.addstudent("Sam","Sammerson","DEF456");
+     gradebook.addstudent("Jess","Jesserson","HIJ789");
+
+
+};
+
+
+
+
+TEST_CASE("Assignment position does not change") {
+     Gradebook gradebook;
+     gradebook.addstudent("Michael"," ""Jackson","ABC123");
+     gradebook.addassignment("First homework", *"100");
+     gradebook.addassignment("Unit Test", *"100");
+     gradebook.addassignment("Second homework", *"100");
+     gradebook.addassignment("Third homework", *"100");
+
+     string reportanalysis = gradebook.report();
+
 }
